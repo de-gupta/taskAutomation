fc05bc5870b041aab6ee4c609d64198af315f725
shylock <shylock@apollo>
2026-06-08 12:49:56 +0200

    cleanup

--- src/main/java/de/gupta/automation/task/framework/cli/FrameworkCliApplication.java
+++ src/main/java/de/gupta/automation/task/framework/cli/FrameworkCliApplication.java
@@ -5,17 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication(proxyBeanMethods = false)
 @ComponentScan(
 		basePackages = {
 				"de.gupta.automation.task.framework",
 				"de.gupta.automation.task.implementations"
-		},
-		excludeFilters = @ComponentScan.Filter(
-				type = FilterType.REGEX,
-				pattern = "de\\.gupta\\.automation\\.task\\.implementations\\..*\\.manual\\..*"))
+		}
+)
 public final class FrameworkCliApplication
 {
 	public static void main(final String[] args)
--- src/main/java/de/gupta/automation/task/framework/rest/FrameworkRestApplication.java
+++ src/main/java/de/gupta/automation/task/framework/rest/FrameworkRestApplication.java
@@ -3,17 +3,14 @@ package de.gupta.automation.task.framework.rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication(proxyBeanMethods = false)
 @ComponentScan(
 		basePackages = {
 				"de.gupta.automation.task.framework",
 				"de.gupta.automation.task.implementations"
-		},
-		excludeFilters = @ComponentScan.Filter(
-				type = FilterType.REGEX,
-				pattern = "de\\.gupta\\.automation\\.task\\.implementations\\..*\\.manual\\..*"))
+		}
+)
 public final class FrameworkRestApplication
 {
 	public static void main(final String[] args)
